15181344268e994b3c1a7c68be9a5b99269ceb83
Aleksey Malakhov <aleksey.malakhov11@gmail.com>
2023-02-21 18:09:14 +0300

    Revert "implemented function to visualize recommended images"
    This reverts commit 46293fb6a3206606a397e5e2cd0f2fc4759b9b6d.

--- utils.py
+++ utils.py
@@ -3,11 +3,6 @@ from PIL import Image
 from pathlib import Path
 import os
 from functools import lru_cache
-from typing import Dict
-import numpy as np
-import matplotlib.pyplot as plt
-from PIL import Image
-import gc
 
 
 class Embedder:
@@ -41,37 +36,3 @@ def load_model(model: torch.nn.Module, device: torch.device) -> torch.nn.Module:
     model.to(device)
     model.eval()
     return model
-
-
-def visualize_recs(
-    idx: int,
-    dataset: torch.utils.data.Dataset,
-    device: torch.device,
-    embedder: torch.nn.Module,
-    embeddings_idxs: Dict[Path, int],
-    top_k: int = 6,
-):
-    item, idx = dataset.__getitem__(idx)
-    item = item.unsqueeze(0).to(device)
-    i = Image.open(dataset._dataset[idx])
-    plt.figure(figsize=(5, 2))
-    plt.imshow(i)
-    plt.title("Main image")
-    plt.show()
-    candidates = []
-    user = embedder.get_embeddings(item)
-    bs = user.size(0)
-    user = user.view(bs, -1)  # B x Embedding_dim
-    item_candidates = []
-    idx_candidates = []
-
-    for embedding_file in os.listdir("/kaggle/input/yandex-embeddings"):
-        item_embeddings = np.load("/kaggle/input/yandex-embeddings/" + embedding_file)
-        item_embeddings = torch.from_numpy(item_embeddings).to(device)
-        ranks = torch.nn.functional.cosine_similarity(user, item_embeddings, dim=1)  # C
-        idxs = torch.argsort(ranks, dim=0, descending=True)
-        item_candidates.append(ranks[idxs[:top_k]].cpu().detach().numpy())
-        idxs += embeddings_idxs[embedding_file]
-        idx_candidates.append(idxs[:top_k].cpu().numpy())
-        del item_embeddings
-        gc.collect()
